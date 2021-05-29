7f626af1cda9c3b1edd9d4cdd50268757663afe4
Daniel Fernandez <dfernandez@newrelic.com>
2021-05-29 16:38:08 +0200

    Bump Gatsby version to V3

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,7 +10,13 @@ import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
-function SEO({ description, lang, meta, title, thumbnail }) {
+function SearchEngineOptimization({
+  description,
+  lang,
+  meta,
+  title,
+  thumbnail
+}) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -26,73 +32,73 @@ function SEO({ description, lang, meta, title, thumbnail }) {
   );
 
   const metaDescription = description || site.siteMetadata.description;
-  const imageSrc = `https://www.danifdz.dev${thumbnail?.childImageSharp.sizes.src}`;
+  const imageSrc = `https://www.danifdz.dev${thumbnail?.childImageSharp.fluid.src}`;
 
   return (
     <Helmet
       htmlAttributes={{
-        lang,
+        lang
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
           name: `description`,
-          content: metaDescription,
+          content: metaDescription
         },
         {
           property: `og:title`,
-          content: title,
+          content: title
         },
         {
           property: `og:description`,
-          content: metaDescription,
+          content: metaDescription
         },
         {
           property: `og:type`,
-          content: `website`,
+          content: `website`
         },
         {
           property: `og:image`,
-          content: imageSrc,
+          content: imageSrc
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary`
         },
         {
           name: `twitter:creator`,
-          content: site.siteMetadata.author,
+          content: site.siteMetadata.author
         },
         {
           name: `twitter:title`,
-          content: title,
+          content: title
         },
         {
           name: `twitter:description`,
-          content: metaDescription,
+          content: metaDescription
         },
         {
           name: `twitter:image`,
-          content: imageSrc,
-        },
+          content: imageSrc
+        }
       ].concat(meta)}
     />
   );
 }
 
-SEO.defaultProps = {
+SearchEngineOptimization.defaultProps = {
   lang: `en`,
   meta: [],
-  description: ``,
+  description: ``
 };
 
-SEO.propTypes = {
+SearchEngineOptimization.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
-  thumbnail: PropTypes.object,
+  thumbnail: PropTypes.object
 };
 
-export default SEO;
+export default SearchEngineOptimization;
